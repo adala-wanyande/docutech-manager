fa9fd1ef45bcbc3424313fd65b6abf609ee7e0a3
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-05-26 11:11:37 +0300

    Updated user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,50 +1,55 @@
 class UsersController < ApplicationController
-    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-    before_action :set_user, only: [:show, :update, :destroy]
-  
-    def index
-      users = User.all
-      render json: users, each_serializer: UserSerializer
-    end
-  
-    def show
-      render json: user, serializer: UserSerializer
-    end
-  
-    def create
-      user = User.create!(user_params)
-      if user.save
-        render json: user, status: :created, serializer: UserSerializer
-      else
-        render json: { error: user.errors.full_messages }, status: :unprocessable_entity
-      end
-    end
-  
-    def update
-      if user.update!(user_params)
-        render json: user, serializer: UserSerializer
-      else
-        render json: { error: user.errors.full_messages }, status: :unprocessable_entity
-      end
-    end
-  
-    def destroy
-      user.destroy
-      head :no_content
-    end
-  
-    private
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  wrap_parameters format: []
 
-    def render_not_found_response
-        render json: { error: "user not found" }, status: :not_found
-    end
-  
-    def set_user
-      user = User.find(params[:id])
-    end
-  
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :role)
+  def index
+    users = User.all
+    render json: users, each_serializer: UserSerializer
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user, serializer: UserSerializer
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "User not found" }, status: :not_found
+  end
+
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      render json: user, status: :created, serializer: UserSerializer
+    else
+      render json: { error: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
-  
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: user, serializer: UserSerializer
+    else
+      render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "User not found" }, status: :not_found
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    head :no_content
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "User not found" }, status: :not_found
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :role)
+  end
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
+  end
+end
