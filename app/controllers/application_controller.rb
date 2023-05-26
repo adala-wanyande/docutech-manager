d494e9e609a2f81c152fbdfef7b7ee7621663d18
Sirumba <simonsmaina45@gmail.com>
2023-05-26 16:32:49 +0300

    hash data into webtoken

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,3 +8,17 @@ def app_response(message: 'success', status: 200, data: nil)
         data: data
     }, status: status
 end
+ # hash data into webtoken
+ def encode(uid)
+    payload={
+        data:{
+            uid: uid
+        },
+        exp: Time.now.to_i + (6*3600)
+    }
+    begin
+        JWT.encode(payload, ENV['task_bit_key'], 'HS256')
+    rescue JWT::EncodeError => e
+        app_response(message: 'failed', status: 400, data: { info: 'Something went wrong. Please try again' })
+    end
+end
