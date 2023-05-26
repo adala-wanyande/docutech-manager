0c30216edad3986fdec8f2d1ff878e9ef688824d
Sirumba <simonsmaina45@gmail.com>
2023-05-26 16:45:27 +0300

    defined the routes in the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
-  resources :todos
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  # user
+  post '/users', to: 'users#register'
+  post '/users/login', to: 'users#login'
+  get '/users/login/check', to: 'users#show'
+  delete '/users/logout', to: 'users#logout'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  # todo
+  post '/todos', to: 'todos#create'
+  put '/todos/:id', to: 'todos#update'
+  get '/todos/:id', to: 'todos#show'
+  delete '/todos/:id', to: 'todos#destroy'
+  get '/todos', to: 'todos#index'
+
+  # verify_auth
+  get '/verify', to: 'application#verify_auth'
 end
