66a30bdc6bdcfa5b2309b9415feedc527c20c3ad
Sirumba <simonsmaina45@gmail.com>
2023-05-27 11:56:42 +0300

    changes in todo.rb

--- api/app/models/todo.rb
+++ api/app/models/todo.rb
@@ -1,2 +1,21 @@
 class Todo < ApplicationRecord
-end
+    belongs_to :user
+    enum :status, [:CREATED, :STARTED, :COMPLETED, :CANCELLED]
+    enum :priority, [:LOW, :MEDIUM, :HIGH]
+  
+    validates :title, {
+      length: {minimum: 5, maximum: 25},
+      presence: true
+    }
+    validates :description, {
+      length: {minimum: 20},
+      presence: true
+    }
+    # validates :status, {
+    #   numericality: {in: 0..3}
+  
+    # }
+    # validates :priority, {
+    #   numericality: {in: 0..2}
+    # }
+  end
